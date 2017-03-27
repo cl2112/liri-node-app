4362cac674dd9605922831d95b0a935e569b9c91
cl2112 <cl2112cl2112@gmail.com>
2017-03-27 16:17:56 -0400

    updated the layout of the code

--- liri.js
+++ liri.js
@@ -1,15 +1,17 @@
+// Requires
 const keys = require("./keys.js");
 const Twitter = require("twitter");
 
+//
 const consumerKey = keys.twitterKeys.consumer_key;
 const consumerSecret = keys.twitterKeys.consumer_secret;
 const accessKey = keys.twitterKeys.access_token_key;
 const accessSecret = keys.twitterKeys.access_token_secret;
 
-console.log(consumerKey);
-console.log(consumerSecret);
-console.log(accessKey);
-console.log(accessSecret);
+// console.log(consumerKey);
+// console.log(consumerSecret);
+// console.log(accessKey);
+// console.log(accessSecret);
 
 
 var command = process.argv[2];
@@ -22,23 +24,6 @@ var client = new Twitter({
   access_token_secret: accessSecret
 });
 
-client.get('statuses/home_timeline', function(error, tweets, response) {
-  if(error) throw error;
-  for (var i = 0; i < tweets.length; i++) {
-  	var name = tweets[i].user.name;
- 	var text = tweets[i].text;
-
- 	console.log("  ");
- 	console.log("-----------------------");
-	console.log(name);
-	console.log(text);
-	console.log("=======================");
-  }
-  
-  
-  //console.log(tweets);  // The favorites. 
-  //console.log(response);  // Raw response object. 
-});
 
 switch (command) {
 	case "my-tweets":
@@ -57,3 +42,26 @@ switch (command) {
 		console.log("nothing entered or there was a spelling error.");
 		break;
 }
+
+
+
+
+
+
+function myTweets() {
+	client.get('statuses/home_timeline', function(error, tweets, response) {
+  		if(error) throw error;
+  		for (var i = 0; i < tweets.length; i++) {
+  			var name = tweets[i].user.name;
+ 			var text = tweets[i].text;
+
+ 			console.log("  ");
+ 			console.log("-----------------------");
+			console.log(name);
+			console.log(text);
+			console.log("=======================");
+  		}  
+	//console.log(tweets);  // The favorites. 
+	//console.log(response);  // Raw response object. 
+	});
+};
